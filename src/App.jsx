8467d0ab12e49aa5de408470308208e75ffac0b8
Arun Shukla <69161719+anshu1016@users.noreply.github.com>
2024-09-28 21:14:07 +0530

    Updated RepoCount

--- src/App.jsx
+++ src/App.jsx
@@ -143,7 +143,7 @@ function App() {
       : "Last 10 Updated Repositories:"}
   </h2>
   <h2 className="text-2xl font-semibold mb-4">
-    {totalCount && `Total Repositories: ${totalCount}`}
+    {totalCount && `Total Repositories: ${repoList.length}`}
   </h2>
 </div>
 
